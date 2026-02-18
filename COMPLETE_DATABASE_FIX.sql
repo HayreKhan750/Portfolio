e1619338325917c5e98c512b224f094705f2b4b9
Hayredin <hayrecode@gmail.com>
2026-02-18 16:01:41 +0300

    Fix: Add explicit type cast in has_role function

--- COMPLETE_DATABASE_FIX.sql
+++ COMPLETE_DATABASE_FIX.sql
@@ -31,7 +31,7 @@ SET search_path = public
 AS $$
   SELECT EXISTS (
     SELECT 1 FROM public.user_roles
-    WHERE user_id = _user_id AND role = _role
+    WHERE user_id = _user_id AND role = _role::app_role
   )
 $$;
 
